f883be668773884dae39e05d29dac2cecc72d13c
Nickel 13 <nikesushko@gmail.com>
2018-09-22 15:01:03 +0300

    initial

---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const gulp = require('gulp'),
+      sass = require('gulp-sass'),
+      pug = require('gulp-pug'),
+      debug = require('gulp-debug'),
+      del = require('del'),
+      imagemin = require('gulp-imagemin'),
+      autoprefixer = require('gulp-autoprefixer'),
+      browserSync = require('browser-sync').create();
+
+gulp.task('pug', function () {
+  return gulp.src('src/pug/*.pug')
+      .pipe(pug())
+      .pipe(debug({title: 'working on'}))
+      .pipe(gulp.dest('build'))
+});
+
+gulp.task('sass', function () {
+  return gulp.src('src/scss/*.scss')
+      .pipe(sass())
+      .pipe(autoprefixer())
+      .pipe(debug({title: 'working on'}))
+      .pipe(gulp.dest('build/css'))
+});
+
+gulp.task('js', function () {
+  return gulp.src('src/js/**/*.js')
+      .pipe(debug({title: 'working on'}))
+      .pipe(gulp.dest('build/js/'))
+});
+
+gulp.task('img', function () {
+  return gulp.src('src/img/**/*.*', {since: gulp.lastRun('img')})
+//   return gulp.src('src/img/**/*.*')
+      .pipe(debug({title: 'working on'}))
+    //   .pipe(imagemin())
+      .pipe(gulp.dest('build/img'))
+});
+
+gulp.task('assets', function () {
+  return gulp.src('src/assets/**/**.*')
+      .pipe(gulp.dest('build/assets'))
+});
+
+gulp.task('browser-sync', function () {
+  browserSync.init({
+    server: {
+      baseDir: 'build'
+    },
+    notify: true
+  })
+});
+
+gulp.task('watch', function () {
+  gulp.watch('src/scss/**/*.*', gulp.series('sass'));
+  gulp.watch('src/pug/**/*.*', gulp.series('pug'));
+  gulp.watch('src/js/**/*.*', gulp.series('js'));
+
+  gulp.watch('build/**/*.*').on('change', browserSync.reload);
+});
+
+gulp.task('clean', function () {
+  return del('build');
+});
+
+gulp.task('build', gulp.series('clean', gulp.parallel('pug', 'sass', 'js', 'img', 'assets')));
+
+gulp.task('serve', gulp.parallel('watch', 'browser-sync'));
+
+gulp.task('dev', gulp.series('build', 'serve'));
